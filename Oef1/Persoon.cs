9ecc7422488660a842ca56f600102ecf9c138dbb
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-10-28 16:06:39 +0200

    Add files via upload

---
+++ Oef1/Persoon.cs
@@ -0,0 +1,21 @@
+﻿namespace Oef1
+{
+    internal class Persoon
+    {
+        private string Naam { get; set; }
+        private int Leeftijd { get; set; }
+
+        public Persoon(string naam, int leeftijd)
+        {
+            Naam = naam;
+            Leeftijd = leeftijd;
+        }
+        public override string ToString()
+        {
+            return "ik noem " + Naam + " en ik ben " + Leeftijd;
+        }
+
+        
+       
+    }
+}
